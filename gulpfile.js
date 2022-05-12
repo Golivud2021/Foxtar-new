0e301e241db0293fdd7614c27c267ff4ae994b81
Lion <78700156+Golivud2021@users.noreply.github.com>
2022-05-12 20:34:02 +0300

    adeed header

--- gulpfile.js
+++ gulpfile.js
@@ -35,8 +35,9 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('js', function(){
-    return gulp.src(['node_modules/@fancyapps/fancybox/distjquery.fancybox.js',
-    'node_modules/slick-carousel/slick/slick.js',
+    return gulp.src(
+        ['node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        'node_modules/slick-carousel/slick/slick.js',
         'node_modules/mixitup/dist/mixitup.js'])
         .pipe(concat('libs.min.js'))
         .pipe(uglifyjs())
